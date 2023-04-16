aa926a42e22946b920c35ce376f70f1878ec1cf6
unknown <hasirnouman76@gmail.com>
2023-04-17 04:33:01 +0500

    baseURL updated

--- utils/axios.ts
+++ utils/axios.ts
@@ -6,13 +6,13 @@ if (typeof window !== "undefined") {
   accessToken = localStorage.getItem("token");
 }
 export const axiosInstance = axios.create({
-  baseURL: local,
+  baseURL: prod,
   headers: {
     "Content-Type": "application/json",
   },
 });
 export const protectedInstance = axios.create({
-  baseURL: local,
+  baseURL: prod,
   headers: {
     Authorization: `Bearer ${accessToken}`,
   },
